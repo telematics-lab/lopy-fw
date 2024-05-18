4e3add5f7229736f1d04902bcde381a689bce3bc
dpugliese6 <daniele.pugliese@poliba.it>
2024-05-18 22:17:29 +0200

    Revise node1 code

--- node1/main.py
+++ node1/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright (c) 2019, Pycom Limited.
+# Copyright (C) 2024, Telematics Lab - Politecnico di Bari.
 #
 # This software is licensed under the GNU GPL version 3 or any
 # later version, with permitted additional terms. For more information
@@ -46,10 +47,11 @@ dev_eui = binascii.unhexlify('xxxx')
 app_eui = binascii.unhexlify('0000000000000000')
 app_key = binascii.unhexlify('xxxx')
 
-# set the 3 default channels to the same frequency (must be before sending the OTAA join request)
-lora.add_channel(0, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
-lora.add_channel(1, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
-lora.add_channel(2, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
+if config.SINGLE_CHANNEL:
+    # set the 3 default channels to the same frequency (must be before sending the OTAA join request)
+    lora.add_channel(0, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
+    lora.add_channel(1, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
+    lora.add_channel(2, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
 
 # join a network using OTAA
 lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0, dr=config.LORA_NODE_DR)
@@ -59,9 +61,14 @@ while not lora.has_joined():
     time.sleep(2.5)
     print('Not joined yet...')
 
-# remove all the non-default channels
-for i in range(3, 16):
-    lora.remove_channel(i)
+if config.SINGLE_CHANNEL:
+    # remove all the non-default channels
+    for i in range(3, 16):
+        lora.remove_channel(i)
+else:
+    # remove all the non-default channels
+    for i in range(8, 16):
+        lora.remove_channel(i)
 
 # create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
@@ -78,12 +85,9 @@ time.sleep(5.0)
 for i in range (200):
     pycom.heartbeat(False)
     pycom.rgbled(0xffff00)
-    lt = LTR329ALS01(py) # Returns height in meters. Mode may also be set to PRESSURE, returning a value in Pascal
-    #print(lt.light())
+    lt = LTR329ALS01(py)
     print("LTR329ALS01 light: " + str(lt.light()))
     li = float('.'.join(str(elem) for elem in lt.light()))
-    #print(dir(lt.light()))
-
     li = li/10
     li= round(li)
     print(li)
@@ -92,11 +96,6 @@ for i in range (200):
     pressure=pr.pressure()*10
     pressure=round(pressure)
     print(pressure)
-    #si = SI7006A20(py)
-    #print("Relative Humidity: " + str(si.humidity()) + " %RH")
-    #= si.humidity()*10
-    #humidity = round(humidity)
-    # print(humidity)
     datal = bytearray(struct.pack("H",li))
     print(datal)
     datam = bytearray(struct.pack("L", pressure))
